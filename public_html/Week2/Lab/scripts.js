fa970a92407f6965ec33640fbdae8d3e22b30403
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-01-15 20:24:54 -0500

    week2

--- public_html/Week2/Lab/scripts.js
+++ public_html/Week2/Lab/scripts.js
@@ -7,6 +7,21 @@
 
 function submitForm() {
     
+    var fullname = document.getElementById("name");
+    var fullnameErr = document.getElementById("err_name");
+    
+    
+    if ( fullname.value.length > 0 ) {
+        fullname.className = "good";
+        fullnameErr.innerHTML = "Ok!";
+        fullnameErr.className = "error";
+    } else {
+        fullname.className = "bad";
+        fullnameErr.innerHTML = "Full name is not entered.";
+        fullnameErr.className = "error";
+    }
+    
+    
     
 }
 
