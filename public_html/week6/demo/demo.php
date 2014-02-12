eba6e71423e2928b883359ece73b36c36e1105ea
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-02-12 18:50:27 -0500

    week6

---
+++ public_html/week6/demo/demo.php
@@ -0,0 +1,269 @@
+<?php
+
+
+$page = $_GET["page"];
+
+if ( $page == 1 || empty($page) ) {
+
+echo <<< END
+
+<blockquote> 
+          <div align="left" name="intro"><font face="Verdana, Arial, Helvetica, sans-serif" color="#999999"><b><a name="intro"></a><font color="#339999">History 
+            of the College</font></b></font></div>
+          <blockquote> 
+            <div align="left"> 
+              <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">For 
+                over seventy years, New England Institute of Technology has met 
+                the challenge of preparing its graduates to enter an increasingly 
+                competitive workplace. Founded in 1940 as the New England Technical 
+                Institute, the present college began as a certificate-granting 
+                trade school. In 1971, a new administration, under the leadership 
+                of President Richard I. Gouse, envisioned a degree-granting institution 
+                that would not only be better qualified to meet industry’s 
+                need for technically-trained workers, but would also become one 
+                of the top technical colleges in the nation.</font></p>
+              <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">When 
+                the Board of Regents of the State of Rhode Island granted the 
+                institution the authority to offer associate degrees in 1977, 
+                its name was changed to <strong>New England Institute of Technology</strong> 
+                (<strong>NEIT</strong>). In 1982, <strong>NEIT </strong>was accredited 
+                by the New England Association of Schools and Colleges (NEASC). 
+                Two years later, in addition to associate degrees, the State of 
+                Rhode Island authorized <strong>NEIT</strong> to confer baccalaureate 
+                degrees and such other degrees, earned and honorary, as are usually 
+                conferred by colleges and universities in addition to its associate 
+                degree programs. By 1990, four new bachelor’s programs had 
+                been added to the College’s offerings. In 1995, the New 
+                England Association of Schools and Colleges’ Commission 
+                on Technical and Career Institutions awarded accreditation to 
+                <strong>NEIT</strong> as a baccalaureate-granting institution. 
+                In 2005, the New England Association of Schools and Colleges continued 
+                <strong>NEIT</strong>'s accreditation through its Commission on 
+                Institutions of Higher Education. In 2010, the College began enrolling 
+                students for its first master of science degree program.</font></p>
+            </div>
+            <div align="left"> 
+              <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">Today, 
+                enrollment has grown to more than 2,800 students from the 70 students 
+                enrolled in 1971. The original educational programs have increased 
+                to over 40 associate, bachelor’s and master’s degree 
+                programs in such diverse fields as automotive technology, business 
+                management, criminal justice, information technology, electronics 
+                systems engineering technology, mechanical engineering technology, 
+                nursing, occupational therapy, surgical technology, and video 
+                and audio production. The campus that started in an older mill-style 
+                building in Providence, Rhode Island, now encompasses three locations; 
+                two in Warwick and one in East Greenwich, Rhode Island, and includes 
+                seven buildings with approximately 500,000 square feet of state-of-the-art 
+                classroom, laboratory, faculty and administrative office space.</font></p>
+              <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">These 
+                dynamic changes to its program offerings and physical plant symbolize 
+                <strong>NEIT</strong>’s vanguard journey into the 21st century 
+                and its response to the radical transformation that new technologies 
+                have brought to the workplace. The needs of tomorrow’s students 
+                demand access to those technological resources that will enable 
+                them to realize technology’s full potential to change their 
+                education and their lives. As it has since its beginning, <strong>NEIT</strong> 
+                has chosen to meet that challenge: the success of the College 
+                will continue to be measured by its graduates’ accomplishments 
+                in the world they are entering both as workers and citizens.</font></p>
+            </div>
+          </blockquote>
+        </blockquote>
+
+END;
+
+
+}
+
+if ( $page == 2 ) {
+echo <<< END
+<blockquote> 
+          <blockquote> 
+            <div align="right"> 
+              <div align="left"><font face="Verdana, Arial, Helvetica, sans-serif"><font color="#339999"><strong> 
+                <a name="statement"></a>Statement of Mission and Objectives</strong></font></font> 
+              </div>
+            </div>
+            <div align="right"> <font size="-1" face="Verdana, Arial, Helvetica, sans-serif"><br>
+              </font> 
+              <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">The 
+                mission of <strong>New England Institute of Technology (NEIT)</strong> 
+                is to provide specialized associate, bachelor, and master’s 
+                degree programs which prepare students for professional and technical 
+                careers. Through the combination of professional and technical 
+                courses and an integrated liberal arts core, academic programs 
+                emphasize the relevance of continuous learning to personal and 
+                professional growth. Upon successful completion, graduates are 
+                prepared to enter the workforce or to continue their education. 
+                As an extension of the primary mission, <strong>NEIT </strong>offers 
+                the opportunity to pursue professional and technological studies 
+                to satisfy personal interests. </font></div>
+            </div>
+            <div align="right"> 
+              <div align="left"> 
+                <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">As 
+                  an institution primarily devoted to teaching and through its 
+                  open admission policy, <strong>NEIT</strong> provides opportunities 
+                  for students from diverse educational backgrounds and with varying 
+                  levels of ability to study in a variety of technological fields.</font></p>
+                <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif"><strong>NEIT</strong>'s 
+                  objectives are to:</font></p>
+              </div>
+            </div>
+            <blockquote> 
+              <ol>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">offer 
+                      technology programs that meet the career-oriented needs 
+                      of students and to assist each student in identifying and 
+                      developing a career path of interest.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">stress 
+                      the relationship between theoretical knowledge and practical 
+                      application.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">provide 
+                      a laboratory approach, which prepares students for business 
+                      and industry.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">enable 
+                      students to develop skills in writing, oral communication, 
+                      and team building.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">use 
+                      innovative learning approaches to help each student realize 
+                      his or her academic potential.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">help 
+                      students to develop skills in problem solving and in thinking 
+                      logically, flexibly, and critically.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">foster 
+                      the student's potential to contribute to and participate 
+                      in a rapidly changing technological society.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">provide 
+                      a campus climate where students are encouraged to respect 
+                      the value of all people and to deal with the social problems 
+                      and responsibilities they face as members of society.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">encourage 
+                      students to develop pride in the quality of their work.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">create 
+                      an appreciation for both the importance and joy of learning.</font></div>
+                  </div>
+                </li>
+                <li> 
+                  <div align="right"> 
+                    <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">be 
+                      environmentally responsible in the development and maintenance 
+                      of its campus and to include environmentally responsible 
+                      technologies in its curriculum. </font> </div>
+                  </div>
+                </li>
+              </ol>
+            </blockquote>
+          </blockquote>
+        </blockquote>
+END;
+}
+
+
+
+if ( $page == 3 ) {
+echo <<< END
+<blockquote> 
+          <blockquote> 
+            <div align="left" name="edphil"><font face="Verdana, Arial, Helvetica, sans-serif"><a name="edphil"></a><b><font color="#339999">Our 
+              Educational Philosophy</font></b></font></div>
+            <div align="left"><font size="-1" face="Verdana, Arial, Helvetica, sans-serif"><strong>New 
+              England Institute of Technology</strong> is a private, non-profit, 
+              technical college whose mission is to provide specialized associate, 
+              bachelor’s, and master’s degree programs for students 
+              from diverse educational backgrounds and with differing levels of 
+              ability. The College’s mission evolved from the principle 
+              that all persons, regardless of age, gender, disability, socioeconomic 
+              circumstances, religious, racial or ethnic background, should have 
+              access to career opportunities through a quality technical education. 
+              To achieve this purpose, <strong>NEIT</strong> is first and foremost 
+              an institution committed to teaching. </font><font face="Verdana, Arial, Helvetica, sans-serif"> 
+              <p><font size="-1">At <strong>NEIT</strong>, we believe and affirm 
+                that every student can learn; we recognize that different students 
+                may learn in different ways with differing levels of ability; 
+                and we recognize the importance of creating a learning environment 
+                in each classroom and laboratory that both challenges and supports 
+                each of our students.</font></p>
+              </font> </div>
+          </blockquote>
+        </blockquote>
+END;
+}
+
+
+if ( $page == 4 ) {
+echo <<< END
+<div align="left"> 
+              <p><font face="Verdana, Arial, Helvetica, sans-serif"><b><font color="#339999"><a name="definition"></a>Definition 
+                of an Educated Person</font></b></font></p>
+              <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">The 
+                New England Institute of Technology philosophy of education described 
+                in its Educational Philosophy is embraced firmly by faculty and 
+                staff. This philosophy is guided and sustained by the <strong>NEIT</strong> 
+                Statement of Mission and Objectives. It is intrinsic to the culture 
+                of the College and it is manifest in how and what the faculty 
+                teach and in what they expect of <strong>NEIT</strong> graduates 
+                as educated persons.</font></p>
+              <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif"><strong>NEIT</strong>’s 
+                graduates achieve technical competence, and develop the values 
+                and standards of professional and ethical behavior that can serve 
+                them well in both the workplace and in our democratic society. 
+                These values include responsible citizenship, an appreciation 
+                of and respect for cultural diversity, and an informed world-view.</font></p>
+              <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif"><strong>NEIT</strong> 
+                anticipates that its graduates know how to observe and to read 
+                carefully, to analyze and evaluate facts and ideas, to reach valid 
+                and well-supported conclusions, and to communicate clearly. The 
+                College expects also that its graduates appreciate other fields 
+                of knowledge &ndash; science, the arts and humanities, and the 
+                social sciences &ndash; and that they are able to integrate what 
+                they have learned from these subjects within their technical fields.</font></p>
+              <p><font size="-1" face="Verdana, Arial, Helvetica, sans-serif">The 
+                faculty strives to instill in their students the belief that knowledge 
+                coupled with experience leads to wisdom, and that education does 
+                not end with a degree, but continues throughout one’s lifetime. 
+                In the process, <strong>NEIT</strong> graduates may discover the 
+                joy to be found in learning, the hallmark of an educated person.</font></p>
+            </div>
+END;
+}
+?>		
